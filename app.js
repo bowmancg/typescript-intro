c32a54963dcbd25bb2ec48b4a05722f3e5c0d25f
Chandler Bowman <doctorgrant99@gmail.com>
2023-08-16 15:37:04 -0600

    added compiler

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'Chandler';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('Something went wrong.', 500);
-console.log(result);
+"use strict";
