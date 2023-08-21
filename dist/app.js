d3908dd024a299463f6f5bd3211d3831f34c2eb0
Chandler Bowman <doctorgrant99@gmail.com>
2023-08-21 14:23:44 -0600

    compressed department properties

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,25 @@
 "use strict";
 var Department = (function () {
-    function Department(n) {
-        this.name = n;
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
     }
+    Department.prototype.describe = function () {
+        console.log("Department (".concat(this.id, "): ").concat(this.name));
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInfo = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
-var accounting = new Department('Accounting');
-console.log(accounting);
+var accounting = new Department('d1', 'Accounting');
+accounting.addEmployee('Chandler');
+accounting.addEmployee('Grant');
+accounting.describe();
+accounting.printEmployeeInfo();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 class Department {
-    name: string
+    private employees: string[] = []
 
-    constructor(n: string) {
-        this.name = n
+    constructor(private readonly id: string, public name: string) {
+
+    }
+
+    describe(this: Department) {
+        console.log(`Department (${this.id}): ${this.name}`)
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInfo() {
+        console.log(this.employees.length)
+        console.log(this.employees)
     }
 }
 
-const accounting = new Department('Accounting')
-console.log(accounting)
+const accounting = new Department('d1', 'Accounting')
+
+accounting.addEmployee('Chandler')
+accounting.addEmployee('Grant')
+
+accounting.describe()
+accounting.printEmployeeInfo()
