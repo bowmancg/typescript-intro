523c961cf2b7e6df1892772e17d1677f43797d2e
Chandler Bowman <doctorgrant99@gmail.com>
2023-08-22 13:56:38 -0600

    adding discriminated unions

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,62 @@ var e1 = {
     privileges: ['create-server'],
     startDate: new Date()
 };
+function addCombine(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInfo(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Starting Date: ' + emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('Car');
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('Truck');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("Loading ".concat(amount, " Lbs. of cargo."));
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
+function moveAnimal(animal) {
+    var speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+    }
+    console.log("A ".concat(animal.type, " moves at ").concat(speed, " mi/hr"));
+}
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
+moveAnimal({ type: 'horse', runningSpeed: 25 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,81 @@ type Combine = string | number
 type Numeric = number | boolean
 
 type Universal = Combine & Numeric
+
+function addCombine(a: Combine, b: Combine) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString()
+    }
+    return a + b
+}
+
+type UnknownEmployee = Employee | Admin
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+    console.log('Name: ' + emp.name)
+    if ('privileges' in emp) {        
+        console.log('Privileges: ' + emp.privileges)
+    }
+    if ('startDate' in emp) {        
+        console.log('Starting Date: ' + emp.startDate)
+    }
+}
+
+printEmployeeInfo(e1)
+
+class Car {
+    drive() {
+        console.log('Car')
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Truck')
+    }
+    loadCargo(amount: number) {
+        console.log(`Loading ${amount} Lbs. of cargo.`)
+    }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive()
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000)
+    }
+}
+
+useVehicle(v1)
+useVehicle(v2)
+
+interface Bird {
+    type: 'bird'
+    flyingSpeed: number
+}
+
+interface Horse {
+    type: 'horse'
+    runningSpeed: number
+}
+
+type Animal = Bird | Horse
+
+function moveAnimal(animal: Animal) {
+    let speed
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed
+            break
+            case 'horse':
+                speed = animal.runningSpeed
+    }
+    console.log(`A ${animal.type} moves at ${speed} mi/hr`)
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10})
+moveAnimal({type: 'horse', runningSpeed: 25})
