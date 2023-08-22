1f8f9c47328e8d288b63ff659ad929ab4626882d
Chandler Bowman <doctorgrant99@gmail.com>
2023-08-22 11:33:46 -0600

    made parameters optional

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,27 @@
 "use strict";
+var addUp;
+addUp = function (n1, n2) {
+    return n1 + n2;
+};
 var Profile = (function () {
     function Profile(n) {
         this.age = 24;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     Profile.prototype.greet = function (phrase) {
-        console.log(phrase + ' ' + this.name);
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log('Hello.');
+        }
     };
     return Profile;
 }());
 var user1;
-user1 = new Profile('Chandler');
+user1 = new Profile();
 user1.greet('My name is');
 console.log(user1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,44 @@
-interface Greeting {
-    name: string
+interface AddFn {
+    (a: number, b: number): number
+}
 
+let addUp: AddFn
+
+addUp = (n1: number, n2: number) => {
+    return n1 + n2
+}
+
+interface Named {
+    readonly name?: string
+    outputName?: string
+}
+
+interface Greeting extends Named {
     greet(phrase: string): void
 }
 
 class Profile implements Greeting {
-    name: string
+    name?: string
     age = 24
 
-    constructor(n: string) {
-        this.name = n
+    constructor(n?: string) {
+        if (n) {            
+            this.name = n
+        }
     }
 
     greet(phrase: string) {
-        console.log(phrase + ' ' + this.name)
+        if (this.name) {            
+            console.log(phrase + ' ' + this.name)
+        } else {
+            console.log('Hello.')
+        }
     }
 }
 
 let user1: Greeting
 
-user1 = new Profile('Chandler')
+user1 = new Profile()
 
 user1.greet('My name is')
 console.log(user1)
