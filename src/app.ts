7dabb0ab549ef510882ba5c2c8a20b30fb9f3172
Chandler Bowman <doctorgrant99@gmail.com>
2023-08-23 12:41:05 -0600

    added a constraint

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,9 @@
 //     data.split(' ')
 // })
 
-function merge<T extends object, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
-  { name: "Chandler", hobbies: ["Books"] },
-  { age: 24 }
-);
+const mergedObj = merge({ name: "Chandler", hobbies: ["Books"] }, { age: 24 });
 console.log(mergedObj);
