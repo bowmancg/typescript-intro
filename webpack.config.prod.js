efae56e9a46e804c0cdb6c37ea6113f47206ed7f
Chandler Bowman <doctorgrant99@gmail.com>
2023-09-01 13:37:13 -0600

    updated webpack build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,19 +1,12 @@
 const path = require("path");
+const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
+  mode: 'production',
   entry: "./src/app.ts",
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
   },
   devtool: "inline-source-map",
   module: {
@@ -28,4 +21,7 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
 };
