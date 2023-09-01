287e3e9e7a980e5d7650363ef98094a1ba7c585b
Chandler Bowman <doctorgrant99@gmail.com>
2023-09-01 10:56:45 -0600

    adjusting webpack

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Project } from "../models/project.js";
-import Component from "./base-component.js";
-import { autobind } from "../decorators/autobind.js";
-import { Draggable } from "../models/drag.js";
+import { Project } from "../models/project";
+import Component from "./base-component";
+import { autobind } from "../decorators/autobind";
+import { Draggable } from "../models/drag";
 
 export class ProjectItem
   extends Component<HTMLUListElement, HTMLLIElement>
